2580e7ae869a921e2779c51cfd18cebd53c7212a
CAPVOK <capvok@yandex.ru>
2023-12-11 20:49:53 +0300

    new breadcrumbs

--- src/components/Breadcrumps/index.tsx
+++ src/components/Breadcrumps/index.tsx
@@ -2,7 +2,7 @@ import "./Breadcrumps.css";
 import { FC } from "react";
 import { IBreadcrumps } from "./typing.tsx";
 import { Link } from "react-router-dom";
-import { Breadcrumb, CloseButton } from "react-bootstrap";
+import { CloseButton } from "react-bootstrap";
 
 export const Breadcrumps: FC<IBreadcrumps> = (props) => {
   const {
@@ -31,22 +31,13 @@ export const Breadcrumps: FC<IBreadcrumps> = (props) => {
           <CloseButton variant="white" />
         </Link>
       )}
-      <Breadcrumb
-        listProps={{
-          className: "m-0 d-flex align-items-center breadcrump_ol",
-        }}
-      >
-        <Breadcrumb.Item
-          linkAs={Link}
-          linkProps={{ to: "/" }}
-          className="breadcrump"
-        >
-          Главная
-        </Breadcrumb.Item>
-        <Breadcrumb.Item active className="breadcrump active">
-          {name}
-        </Breadcrumb.Item>
-      </Breadcrumb>
+      <div className="breadcrumbs">
+        <ul>
+          <li><Link to={"/"}>Главная</Link></li>
+          <div>/</div>
+          <li>{name}</li>
+        </ul>
+      </div>
     </div>
   );
 };
